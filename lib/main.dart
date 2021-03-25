6efd251c84508543642a057c1d6df82f556c192f
Josseph <jossephalvarez@gmail.com>
2021-03-25 15:33:00 +0100

    feat(): bottom bar added, camera_page.dart added, some functionality added

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,133 @@
-import 'package:flutter/cupertino.dart';
-import 'package:app_poc/ui/pages/result_page.dart';
+/// Flutter code sample for BottomNavigationBar
 
-void main() {
+import 'package:flutter/material.dart';
+import 'package:app_poc/ui/pages/camera_page.dart';
+import 'package:app_poc/ui/pages/result_page.dart';
+import 'package:camera/camera.dart';
+
+List<CameraDescription> cameras;
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  cameras = await availableCameras();
+  print(cameras.length);
   runApp(MyApp());
 }
 
+/// This is the main application widget.
 class MyApp extends StatelessWidget {
+  const MyApp({key}) : super();
+
+  static const String _title = 'Flutter POC APP';
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return CupertinoApp(
-        theme: CupertinoThemeData(primaryColor: CupertinoColors.systemIndigo),
-        title: 'Cam APP',
-        home: ResultPage());
+    return const MaterialApp(
+      title: _title,
+      home: MyStatefulWidget(),
+    );
   }
 }
 
+/// This is the stateful widget that the main application instantiates.
+class MyStatefulWidget extends StatefulWidget {
+  // ignore: non_constant_identifier_names
+  const MyStatefulWidget({Key}) : super();
 
+  @override
+  _MyStatefulWidgetState createState() => _MyStatefulWidgetState();
+}
+
+/// This is the private State class that goes with MyStatefulWidget.
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  int _selectedIndex = 1;
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Index 0: Location',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 1: HOME',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 2: CAMERA',
+      style: optionStyle,
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+/*    setState(() {
+      _selectedIndex = index;
+    });*/
+    print(index);
+    switch (index) {
+      case 0:
+        {
+          print('....0');
+          setState(() {
+            _selectedIndex = index;
+          });
+        }
+        break;
+
+      case 1:
+        {
+          print('....1');
+          setState(() {
+            _selectedIndex = index;
+          });
+        }
+        break;
+      case 2:
+        {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (context) => CameraPage(
+                      cameras: cameras,
+                    )),
+          );
+        }
+        break;
+      default:
+        {
+          print('default');
+        }
+        break;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('FLUTTER POC APP'),
+      ),
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.gps_fixed_outlined),
+            label: 'Location',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.camera_alt_outlined),
+            label: 'Camera',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.lightBlueAccent,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
--- lib/ui/pages/result_page.dart
+++ lib/ui/pages/result_page.dart
@@ -1,48 +1,125 @@
 import 'package:flutter/cupertino.dart';
+import 'package:app_poc/controllers/loading_controller.dart';
+import 'package:app_poc/ui/widgets/loading_widget.dart';
 
 class ResultPage extends StatelessWidget {
+  LoadingController _controller = LoadingController.instance;
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return CupertinoPageScaffold(
-      navigationBar: CupertinoNavigationBar(
-        middle: Text('Result'),
-        leading: GestureDetector(
-          onTap: () {
-            print('ON TAP');
-          },
-          child: Icon(CupertinoIcons.back),
+    return Stack(
+      children: <Widget>[
+        CupertinoPageScaffold(
+          navigationBar: CupertinoNavigationBar(
+            middle: Text('Resultado'),
+            leading: GestureDetector(
+              onTap: () {
+                Navigator.pop(context);
+                print('se presiono el boton');
+              },
+              child: Icon(CupertinoIcons.back),
+            ),
+          ),
+          child: Column(
+            children: <Widget>[
+              SizedBox(height: 68),
+              Expanded(child: _Body()),
+            ],
+          ),
         ),
-      ),
-      child: _Body(),
+        ValueListenableBuilder(
+          valueListenable: _controller.isLoading,
+          builder: (context, value, child) {
+            return value ? LoadingWidget() : Container();
+          },
+        ),
+      ],
     );
   }
 }
 
-class _Body extends StatelessWidget {
-  Widget imageContainer() {
+class _Body extends StatefulWidget {
+  @override
+  __BodyState createState() => __BodyState();
+}
+
+class __BodyState extends State<_Body> {
+  String resultUrl;
+
+  Widget imageContainer(String url) {
     return Container(
-        height: 300,
-        width: 300,
-        decoration: BoxDecoration(
-            color: CupertinoColors.white,
-            borderRadius: BorderRadius.circular(16.0),
-            image: DecorationImage(
-              fit: BoxFit.cover,
-              image: NetworkImage(
-                  "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.HYbn2i5aFs1U2kPenEdUqgHaEK%26pid%3DApi&f=1"),
-            )));
+      height: 300.0,
+      width: 300.0,
+      decoration: BoxDecoration(
+          color: CupertinoColors.white,
+          borderRadius: BorderRadius.circular(16.0),
+          image: DecorationImage(fit: BoxFit.cover, image: NetworkImage(url))),
+    );
+  }
+
+  Widget imageResultContainer() {
+    return Column(
+      children: <Widget>[
+        imageContainer(
+            "https://sumedico.blob.core.windows.net/images/2020/03/17/cuidadosgatobebe_2-focus-min0.07-0.45-983-557.jpg"),
+        SizedBox(height: 16.0),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            CupertinoButton(
+              color: CupertinoTheme.of(context).primaryColor,
+              child: Text(
+                'Descargar',
+                style: TextStyle(color: CupertinoColors.white),
+              ),
+              onPressed: () {},
+            ),
+            CupertinoButton(
+              child: Text('Compartir'),
+              onPressed: () {},
+            )
+          ],
+        )
+      ],
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    resultUrl = "";
   }
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      width: double.infinity,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[imageContainer()],
-      ),
-    );
+        width: double.infinity,
+        child: SingleChildScrollView(
+          physics: BouncingScrollPhysics(),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              imageContainer(
+                  "https://www.purina-latam.com/sites/g/files/auxxlc391/files/styles/facebook_share/public/Que_debes_saber_antes_de_adoptar_un_gatito.jpg?itok=btO4gas0"),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8.0),
+                child: CupertinoButton(
+                  onPressed: () async {
+                    LoadingController.instance.loading();
+                    LoadingController.instance.changeText('Procesando imagen');
+                    await Future.delayed(Duration(seconds: 2), () {});
+                    setState(() {
+                      resultUrl = "imagen con resultado";
+                    });
+                    LoadingController.instance.close();
+                  },
+                  child: Text('Aplicar Filtro'),
+                ),
+              ),
+              resultUrl == "" ? Container() : imageResultContainer()
+            ],
+          ),
+        ));
   }
 }
